48325c1ed4f4feb65d328a23287872192b08b2d8
Luke Weisenbach <weisluke@alum.mit.edu>
2025-04-11 13:24:51 +0100

    initial commit for lightcurve generation

---
+++ microlensing/Lightcurves/lightcurves.py
@@ -0,0 +1,110 @@
+import numpy as np
+from scipy.signal import convolve
+
+from microlensing.IPM.ipm import IPM
+from . import util
+
+
+def constant_source(ipm: IPM, source, positions = 1):
+    '''
+    Return the magnification(s) for the provided magnification map,
+    constant source profile, and position(s)
+
+    :param ipm: formally, an IPM instance that has ran and has a magnification
+                map. in general, any object that has attributes magnifications,
+                center, half_length, and num_pixels, in (y1,y2) coordinates
+    :param source: source object that must contain a 2D kernel as source.profile
+                   and weight (sum of the kernel) as source.weight
+    :param positions: either a position in the magnification map, 
+                      a collection of positions,
+                      or an integer number of random positions
+    '''
+    if np.ndim(source.profile) != 2:
+        raise ValueError("source.profile must be a 2D array")
+    
+    if isinstance(positions, int):
+        positions = util.random_position(ipm, source.profile, positions)
+    else:
+        positions = np.atleast_1d(positions)
+
+        if not util.valid_positions(positions, ipm, source.profile):
+            raise ValueError("provided positions do not lie within the necessary border")
+
+    # convolve map and normalize
+    convolved_map = convolve(ipm.magnifications, source.profile, mode='same') / source.weight
+    interp = util.interpolated_map(convolved_map, ipm.center, 
+                                   ipm.half_length, ipm.num_pixels)
+    
+    magnifications = interp(positions)
+    
+    return magnifications
+
+def changing_source(ipm: IPM, source, positions = 1):
+    '''
+    Return the magnifications for the provided magnification map, 
+    changing source profiles, and position(s)
+
+    :param ipm: formally, an IPM instance that has ran and has a magnification
+                map. in general, any object that has attributes magnifications,
+                center, half_length, and num_pixels, in (y1,y2) coordinates
+    :param source: source object that must contain a list of 2D kernels as source.profiles
+                   and weights (sums of the kernels) as source.weights.
+                   kernels must all have the same shape
+    :param positions: either a position in the magnification map, 
+                      a collection of positions,
+                      or an integer number of random positions
+    '''
+    if np.ndim(source.profiles) != 3:
+        raise ValueError("source.profiles must be a 3D array")
+
+    source_shape = (np.shape(source.profiles)[2], np.shape(source.profiles)[1])
+    
+    if isinstance(positions, int):
+        positions = util.random_position(ipm, source.profiles[-1], positions)
+    else:
+        positions = np.atleast_1d(positions)
+
+        if not util.valid_positions(positions, ipm, source.profiles[-1]):
+            raise ValueError("provided positions do not lie within the necessary border")
+
+    interp = util.interpolated_map(ipm.magnifications, ipm.center, 
+                                   ipm.half_length, ipm.num_pixels)
+    
+    # create 2D array of x and y coordinates of the source profile
+    x, y = np.meshgrid(np.arange(source_shape[0]),
+                       np.arange(source_shape[1]))
+    # add 0.5 to offset to center of pixels
+    x = x + 0.5
+    y = y + 0.5
+    # and recenter at (0,0) by subtracting half the profile size
+    x = x - source_shape[0] / 2
+    y = y - source_shape[1] / 2
+    # convert from pixels to magnification map coordinates
+    x = x * ipm.pixel_scales[0]
+    y = y * ipm.pixel_scales[1]
+
+    # add desired positions to source profile x,y values 
+    # (i.e. recenter source profile at each position)
+    # axes -2 and -1 are now the source profile axes
+    # earlier axes are the collection of positions
+    x = x + np.expand_dims(positions[...,0], (-2,-1))
+    y = y + np.expand_dims(positions[...,1], (-2,-1))
+
+    # add additional (time) axis for the changing profile
+    # axis -3 is time axis, -2 and -1 are still source profile
+    x = np.expand_dims(x, -3)
+    y = np.expand_dims(y, -3)
+
+    # interpolate magnifications on the source profile location grid
+    # in the transpose, we want all axes to stay the same 
+    # EXCEPT the coordinates need to become the last axis
+    # the roll makes sure the transpose moves axes like
+    # [0,1,2,3,...n] -> [1,2,3,...n,0]
+    # multiply and sum over source profile axes
+    magnifications = (np.sum(source.profiles 
+                             * interp(np.transpose([x, y], 
+                                                   axes=np.roll(np.arange(x.ndim + 1),-1))), 
+                             axis=(-2,-1)) 
+                      / source.weights) # and normalize!
+    
+    return magnifications
---
+++ microlensing/Lightcurves/util.py
@@ -0,0 +1,97 @@
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+
+from microlensing.IPM.ipm import IPM
+
+
+def pixel_to_point(pixel, center, half_length, num_pixels):
+    '''
+    convert a pixel coordinate into a point in the source plane
+    parameters may be 1D or 2D values
+
+    :param pixel: pixel coordinate or array of pixel coordinates
+    :param center: center of the magnification map
+    :param half_length: half_length of the magnification map
+    :param num_pixels: number of pixels of the magnification map along the axes
+    '''
+    if not isinstance(pixel, np.ndarray):
+        pixel = np.array(pixel)
+    return pixel * 2 * half_length / num_pixels - half_length + center
+
+def get_borders(ipm: IPM, kernel):
+    '''
+    return the inner border of the magnification map after removing a
+    region the size of the provided kernel around the edge
+
+    :param ipm: formally, an IPM instance that has ran and has a magnification
+                map. in general, any object that has attributes magnifications,
+                center, half_length, and num_pixels, in (y1,y2) coordinates
+    :param kernel: kernel of the source profile    
+    :return: (y1_min, y1_max), (y2_min, y2_max)
+    '''
+    # borders of the valid region in pixel coordinates
+    x = (np.shape(kernel)[1] / 2, ipm.num_pixels[0] - np.shape(kernel)[1] / 2)
+    y = (np.shape(kernel)[0] / 2, ipm.num_pixels[1] - np.shape(kernel)[0] / 2)
+
+    return pixel_to_point(np.transpose([x, y]), ipm.center, 
+                          ipm.half_length, ipm.num_pixels).T
+
+def random_position(ipm: IPM, kernel, num: int = 1):
+    '''
+    return a number of random valid positions in the magnification map
+
+    :param ipm: formally, an IPM instance that has ran and has a magnification
+                map. in general, any object that has attributes magnifications,
+                center, half_length, and num_pixels, in (y1,y2) coordinates
+    :param kernel: kernel of the source profile
+    :param num: number of random positions to return
+    '''
+    # borders of the valid region
+    xlim, ylim = get_borders(ipm, kernel)
+
+    rng = np.random.default_rng()
+    x, y = rng.uniform(*xlim, num), rng.uniform(*ylim, num)
+
+    return np.squeeze(np.transpose([x, y])) # to remove unnecessary dimensions if num=1
+
+def valid_positions(positions, ipm: IPM, kernel):
+    '''
+    determine whether positions within the magnification map are valid
+    to avoid edge effects for the given kernel
+
+    :param positions: positions to check
+    :param ipm: formally, an IPM instance that has ran and has a magnification
+                map. in general, any object that has attributes magnifications,
+                center, half_length, and num_pixels, in (y1,y2) coordinates
+    :param kernel: kernel of the source profile
+    '''
+    if np.ndim(kernel) != 2:
+        raise ValueError("kernel must be a 2D array")
+    
+    if positions.shape[-1] != 2:
+        raise ValueError("There are not 2 coordinates per position")
+    
+    # borders of the valid region
+    xlim, ylim = get_borders(ipm, kernel)
+    x, y = positions[...,0], positions[...,1]
+
+    return (np.all(x > xlim[0]) and np.all(x < xlim[1])
+            and np.all(y > ylim[0]) and np.all(y < ylim[1]))
+
+def interpolated_map(magnifications, center, half_length, num_pixels):
+    '''
+    :param magnifications: magnification map
+    :param center: center of the magnification map
+    :param half_length: half_length of the magnification map
+    :param num_pixels: number of pixels of the magnification map along the axes
+    '''
+    # add 0.5 to offset to center of pixels for interpolation
+    x, y = np.arange(num_pixels[0]) + 0.5, np.arange(num_pixels[1]) + 0.5
+
+    x = pixel_to_point(x, center[0], half_length[0], num_pixels[0])
+    y = pixel_to_point(y, center[1], half_length[1], num_pixels[1])
+
+    # reverse magnification map since (0,0) in pixel coordinates for 
+    # python arrays is top left corner and we want it to be bottom left
+    return RegularGridInterpolator((x,y), magnifications[::-1].T,
+                                   bounds_error=False, fill_value=None)
